1de356bc9bbc56d7b396c744d8bc08d2c625bc5a
ahmedsayed1850 <khaledtoleo1850@gmail.com>
2022-04-15 22:03:02 +0200

    Medium App

---
+++ pages/post/[slug].tsx
@@ -0,0 +1,240 @@
+import React, { useState } from 'react'
+import { sanityClient, urlFor } from '../../sanity'
+import Header from '../../components/Header'
+import type { Post } from '../../typing'
+import { GetStaticProps } from 'next'
+import PortableText from 'react-portable-text'
+import { useForm, SubmitHandler } from 'react-hook-form'
+
+interface IFormInput {
+  _id: string
+  name: string
+  email: string
+  comment: string
+}
+interface Props {
+  post: Post
+}
+const Post = ({ post }: Props) => {
+  const [submitted, setsubmitted] = useState(false)
+  console.log(post)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>()
+
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    fetch('/api/createComment', {
+      method: 'POST',
+      body: JSON.stringify(data),
+    })
+      .then(() => {
+        console.log(data)
+        setsubmitted(true)
+      })
+      .catch((err) => {
+        console.log(err)
+        setsubmitted(false)
+      })
+  }
+  return (
+    <main>
+      <Header />
+
+      <img
+        className="h-40 w-full object-cover"
+        src={urlFor(post.mainImage).url()}
+        alt=""
+      />
+
+      <article className="mx-auto max-w-3xl p-5">
+        <h1 className="mt-10 mb-3 text-3xl">{post.title}</h1>
+        <h2 className="mb-2 text-xl font-light text-gray-500">
+          {post.description}
+        </h2>
+
+        <div className="flex items-center space-x-2">
+          <img
+            className="h-10 w-10 rounded-full"
+            src={urlFor(post.author.image).url()!}
+            alt=""
+          />
+
+          <p className="text-sm font-extralight">
+            Blog post by{' '}
+            <span className="text-green-600">{post.author.name}</span> -
+            Published at {new Date(post._createAt).toLocaleString()}
+          </p>
+        </div>
+
+        <div className="mt-10">
+          <PortableText
+            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
+            content={post.body}
+            serializers={{
+              h1: (props: any) => (
+                <h1 className="my-5 text-2xl font-bold" {...props} />
+              ),
+              h2: (props: any) => (
+                <h1 className="my-5 text-xl font-bold" {...props} />
+              ),
+              li: ({ children }: any) => (
+                <li className="ml-4 list-disc">{children}</li>
+              ),
+              link: ({ href, children }: any) => (
+                <a href={href} className="text-blue-500 hover:underline">
+                  {children}
+                </a>
+              ),
+            }}
+          />
+        </div>
+      </article>
+      <hr className="my-5 mx-auto max-w-lg border border-yellow-500 " />
+      {submitted ? (
+        <div className="my-10 mx-auto flex max-w-2xl flex-col bg-yellow-500 p-10 text-white">
+          <h3 className="text-3xl font-bold">
+            Thanks your comment submitted successfully!
+          </h3>
+          <p>it will be reviewed in a while</p>
+        </div>
+      ) : (
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="mx-auto mb-10 flex max-w-2xl flex-col p-5"
+        >
+          <h3 className="text-sm text-yellow-500">If you enjoyed reading</h3>
+          <h4 className="text-3xl font-bold">Please leave a comment below!</h4>
+          <hr className="mt-2 py-3" />
+          <input
+            {...register('_id')}
+            type="hidden"
+            name="_id"
+            value={post._id}
+          />
+          <label className="mb-5 block">
+            <span className="text-gray-700">Name</span>
+            <input
+              {...register('name', { required: true })}
+              className="form-input mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-0 ring-yellow-500 focus:ring"
+              placeholder="Your Name"
+              type="text"
+            />
+          </label>
+          <label className="mb-5 block">
+            <span className="text-gray-700">Email</span>
+            <input
+              {...register('email', { required: true })}
+              className="form-input mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-0 ring-yellow-500 focus:ring"
+              placeholder="Your Email"
+              type="text"
+            />
+          </label>
+          <label className="mb-5 block">
+            <span className="text-gray-700">Comment</span>
+            <textarea
+              {...register('comment', { required: true })}
+              className="form-textarea mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-0 ring-yellow-500 focus:ring"
+              placeholder="Text area"
+              rows={8}
+            />
+          </label>
+          <div className="flex flex-col p-5">
+            {errors.name && (
+              <span className="text-red-500">- The Name Field is required</span>
+            )}
+            {errors.email && (
+              <span className="text-red-500">
+                - The Email Field is required
+              </span>
+            )}
+            {errors.comment && (
+              <span className="text-red-500">
+                - The Comment Field is required
+              </span>
+            )}
+          </div>
+          <input
+            type="submit"
+            className="focus:shadow-outline cursor-pointer rounded bg-yellow-500 py-2 px-4 font-bold text-white shadow hover:bg-yellow-400 focus:outline-none"
+          />
+        </form>
+      )}
+      <div className="my-10 mx-auto flex max-w-2xl flex-col space-y-2 p-10 shadow shadow-yellow-500">
+        <h3 className="text-4xl">Comments</h3>
+        <hr className="pb-2" />
+
+        <p>
+          {post.comments.map((comment) => (
+            <p>
+              <span className="text-yellow-500"> {comment.name} :</span>{' '}
+              {comment.comment}
+            </p>
+          ))}
+        </p>
+      </div>
+    </main>
+  )
+}
+
+export default Post
+
+export const getStaticPaths = async () => {
+  const query = `*[_type == "post"] {
+            _id,
+            slug {
+                current
+            }
+        }
+    `
+  const posts = await sanityClient.fetch(query)
+  const paths = posts.map((post: Post) => ({
+    params: {
+      slug: post.slug.current,
+    },
+  }))
+
+  return {
+    paths,
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const query = `
+    *[_type == "post" && slug.current == $slug][0] {
+        _id,
+        _createdAt,
+        title,
+        author -> {
+        name,
+        image
+      },
+      "comments": *[
+          _type == "comment" &&
+          post._ref == ^._id &&
+          approved == true],
+      description,
+      mainImage,
+        slug,
+      body
+      }
+    `
+  const post = await sanityClient.fetch(query, {
+    slug: params?.slug,
+  })
+  if (!post) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      post,
+    },
+    revalidate: 60, // will update and cache after 60 seconds please note that 60 seconds is very aggresive and you can make it 4h or a day
+  }
+}
